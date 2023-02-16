11db988c9074e8a61a2b664949838f82973fda33
tzhangm <tzhangm@trip.com>
2023-02-16 14:44:49 +0800

    upd2

--- packages/evaporator-mock-api/src/app.module.ts
+++ packages/evaporator-mock-api/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MockModule} from "./apps/mock/mock.module";
+import {join} from "path";
+// import { join } from 'path'
+import { ServeStaticModule } from '@nestjs/serve-static'
 
 @Module({
-  imports: [MockModule],
+  imports: [MockModule,ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'client'),
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
